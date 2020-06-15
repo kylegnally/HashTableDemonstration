d4a4e74183c527bbf00c5acaf830a046c7fc43eb
Kyle G. Nally <kylegnally@hotmail.com>
2020-06-15 19:02:41 -0400

    Running a mod 99 on the ID array tields interesting results.

--- HashTableDemonstration/Program.cs
+++ HashTableDemonstration/Program.cs
@@ -14,13 +14,32 @@ namespace HashTableDemonstration
 
             OurHashTable<Student> table = new OurHashTable<Student>();
 
-            for (int i = 100; i < 10000; i += 100)
-            {
-                s = new Student();
-                s.ID = (uint)(97 + i);
 
-                table.Add((int)s.ID, s);
+            int[] idArray = new int[]
+            {
+                99999999, 99999999, 75000000, 50000000, 25000, 15000, 10000, 5000, 1
+            } ;
+
+            int[] modulusResults = new int[idArray.Length];
+
+            int i = 0;
+            foreach (int id in idArray)
+            {
+                modulusResults[i] = id % 9;
+                Console.WriteLine(modulusResults[i].ToString() + "\n");
+                i++;
             }
+
+            Environment.Exit(0);
+
+            //for (int i = 0; i < )
+            //for (int i = 100; i < 10000; i += 100)
+            //{
+            //    s = new Student();
+            //    s.ID = (uint)(97 + i);
+
+            //    table.Add((int)s.ID, s);
+            //}
 
             s = new Student();
             s.ID = (uint)(97 + 9800);
